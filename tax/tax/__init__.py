5c3c6621ada88566e7208bfea198e076bb665ba7
henok keraga <henokkeraga@gmail.com>
2021-08-29 11:10:35 -0600

    tAX

---
+++ tax/tax/__init__.py
@@ -0,0 +1,28 @@
+
+
+print("hello ")
+
+factorial = 1
+i = 10
+while i > 0:
+    factorial *= i
+    i -= 1
+print(factorial)
+
+"""
+1 2 3 4 5 
+
+1
+
+"""
+row = 6
+while row >0:
+    col = 1
+    while col < row:
+        print (col , end = " " )
+        col += 1
+    print ("")
+    row -= 1
+# ---------------------
+
+
